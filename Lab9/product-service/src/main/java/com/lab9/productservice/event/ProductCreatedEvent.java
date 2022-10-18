682725136479612f11e4508a5c9977862d23f464
it62070023 <50399016+not10002@users.noreply.github.com>
2022-10-18 16:26:47 +0700

    Add files via upload

---
+++ Lab9/product-service/src/main/java/com/lab9/productservice/event/ProductCreatedEvent.java
@@ -0,0 +1,14 @@
+package com.lab9.productservice.event;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductCreatedEvent {
+
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+}
